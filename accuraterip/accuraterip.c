601f43d6866e017d5b8b9cb6c19d4d3de716ecf5
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2018-05-11 22:53:09 +0200

    reuse generic defines

--- accuraterip/accuraterip.c
+++ accuraterip/accuraterip.c
@@ -10,10 +10,6 @@
 #define HI_U64(x) ((uint32_t)((x)>>32))
 #define LO_U64(x) ((uint32_t)((x)&(uint64_t)0xffffffff))
 
-#define BYTES_PER_SECTOR   (2352)                              /* number of bytes per sector */
-#define BYTES_PER_SAMPLE   (sizeof(uint32_t))                  /* number of bytes per sample */
-#define SAMPLES_PER_SECTOR (BYTES_PER_SECTOR/BYTES_PER_SAMPLE) /* number of samples per sector */
-
 /* keeping this for reference's sake.  Doesn't properly calculate checksums for first or last track.
  * Please use accuraterip_checkum() instead
  */
@@ -29,16 +25,16 @@ uint32_t _accuraterip_checksum_v1_u32(
 	const uint32_t *p = audio_data;  /* current pos in audio sample data */
 	uint32_t audio_num_samples = audio_num_bytes / BYTES_PER_SAMPLE;
 
-	/* skip first 5 seconds in first track */
+	/* skip first 5 frames in first track */
 	if (is_first_track)
 	{
-		p += 5*SAMPLES_PER_SECTOR;
+		p += 5*SAMPLES_PER_FRAME;
 		assert(0); /* not supported */
 	}
 	/* skip last 5 seconds in final track */
 	if (is_last_track)
 	{
-		audio_num_samples -= 5*SAMPLES_PER_SECTOR;
+		audio_num_samples -= 5*SAMPLES_PER_FRAME;
 	}
 
 	uint32_t crc_pos = 1;
@@ -70,13 +66,13 @@ uint32_t _accuraterip_checksum_v2_u32(
 	/* skip first 5 seconds in first track */
 	if (is_first_track)
 	{
-		p += 5*SAMPLES_PER_SECTOR;
+		p += 5*SAMPLES_PER_FRAME;
 		assert(0); /* not supported */
 	}
 	/* skip last 5 seconds in final track */
 	if (is_last_track)
 	{
-		audio_num_samples -= 5*SAMPLES_PER_SECTOR;
+		audio_num_samples -= 5*SAMPLES_PER_FRAME;
 	}
 
 	uint64_t crc_pos = 1;
@@ -119,14 +115,14 @@ int accuraterip_checksum(
 		/* not sure why one would want to skip 5 full sectors minus 1 sample.  Maybe an off-by-one in
 		 * the original implementation?
 		 */
-		audio_start += (5*SAMPLES_PER_SECTOR-1)*num_channels;
-		crc_pos += 5*SAMPLES_PER_SECTOR-1;
-		printf("starting at pos %zu, crc_pos=%u\n", (void*)audio_start-(void*)audio_data, crc_pos);
+		audio_start += (5*SAMPLES_PER_FRAME-1)*num_channels;
+		crc_pos += 5*SAMPLES_PER_FRAME-1;
+		printf("starting at pos %td, crc_pos=%u\n", (void*)audio_start-(void*)audio_data, crc_pos);
 	}
 	if (is_last_track)
 	{
 		/* a the the end, we do skip exactly 5 full sectors */
-		audio_end -= 5*SAMPLES_PER_SECTOR*num_channels;
+		audio_end -= 5*SAMPLES_PER_FRAME*num_channels;
 	}
 
 	uint32_t crc1    = 0;
