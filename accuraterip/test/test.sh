d05182da9716b93656c28f7bc17c0192b8829720
Bas Zoetekouw <bas.zoetekouw@surfnet.nl>
2020-04-01 23:25:57 +0200

    Fix tests to actually work and rework the test script a bit to remove duplicated code

--- accuraterip/test/test.sh
+++ accuraterip/test/test.sh
@@ -7,87 +7,81 @@
 FULL_V2_NORMAL=bfb443d3
 FULL_V2_FIRST=673123e9
 FULL_V2_LAST=bfb443d3
+FULL_V2_SINGLE=673123e9
 
 FULL_V1_NORMAL=e098daf7
 FULL_V1_FIRST=881b8b37
 FULL_V1_LAST=e098daf7
+FULL_V1_SINGLE=881b8b37
 
-SHORT_V2_NORMAL=0360d7e7
-SHORT_V2_FIRST=d2ef1475
-SHORT_V2_LAST=ed0fe3ce
+SHORT_V2_NORMAL=2c5fda79
+SHORT_V2_FIRST=fbee1707
+SHORT_V2_LAST=2a4a6ee3
+SHORT_V2_SINGLE=f9d8ab71
 
-SHORT_V1_NORMAL=d51656cE
-SHORT_V1_FIRST=a4c44438
-SHORT_V1_LAST=e2d97188
+SHORT_V1_NORMAL=f45b5064
+SHORT_V1_FIRST=c4093dce
+SHORT_V1_LAST=3feb027c
+SHORT_V1_SINGLE=0f98efe6
+
+function comp() {
+	MSG=$1
+	OK1=$2
+	OK2=$3
+	CHK1=$4
+	CHK2=$5
+
+	echo -n "Test for ${MSG}... "
+	if ! [ "$OK1" = "$CHK1" -a "$OK2" = "$CHK2" ]
+	then
+		echo "FAILED"
+		echo " - v1: expected $OK1, got $CHK1"
+		echo " - v2: expected $OK2, got $CHK2"
+	else
+		echo "OK"
+	fi
+}
 
 
 # check full file
 chksums=($(../accuraterip full.wav))
-echo -n "Test for full.wav (middle track)... "
-if ! [ ${chksums[0]}=$FULL_V1_NORMAL -a ${chksums[1]}=$FULL_V1_NORMAL ]
-then
-	echo "FAILED"
-	echo " - v1: expected $FULL_V1_NORMAL, got ${chksums[0]}"
-	echo " - v2: expected $FULL_V2_NORMAL, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "full.wav (middle track)... " $FULL_V1_NORMAL $FULL_V2_NORMAL ${chksums[@]}
 
 chksums=($(../accuraterip -f full.wav))
-echo -n "Test for full.wav (first track)... "
-if ! [ ${chksums[0]}=$FULL_V1_FIRST -a ${chksums[1]}=$FULL_V1_FIRST ]
-then
-	echo "FAILED"
-	echo " - v1: expected $FULL_V1_FIRST, got ${chksums[0]}"
-	echo " - v2: expected $FULL_V2_FIRST, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "full.wav (first track)... " $FULL_V1_FIRST $FULL_V2_FIRST ${chksums[@]}
 
 chksums=($(../accuraterip -l full.wav))
-echo -n "Test for full.wav (last track)... "
-if ! [ ${chksums[0]}=$FULL_V1_LAST -a ${chksums[1]}=$FULL_V1_LAST ]
-then
-	echo "FAILED"
-	echo " - v1: expected $FULL_V1_LAST, got ${chksums[0]}"
-	echo " - v2: expected $FULL_V2_LAST, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "full.wav (last track)... " $FULL_V1_LAST $FULL_V2_LAST ${chksums[@]}
+
+chksums=($(../accuraterip -f -l full.wav))
+comp "full.wav (single track)... " $FULL_V1_SINGLE $FULL_V2_SINGLE ${chksums[@]}
 
 
 # check short file
 chksums=($(../accuraterip short.wav))
-echo -n "Test for short.wav (middle track)... "
-if ! [ ${chksums[0]}=$SHORT_V1_NORMAL -a ${chksums[1]}=$SHORT_V1_NORMAL ]
-then
-	echo "FAILED"
-	echo " - v1: expected $SHORT_V1_NORMAL, got ${chksums[0]}"
-	echo " - v2: expected $SHORT_V2_NORMAL, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "short.wav (middle track)... " $SHORT_V1_NORMAL $SHORT_V2_NORMAL ${chksums[@]}
 
 chksums=($(../accuraterip -f short.wav))
-echo -n "Test for short.wav (first track)... "
-if ! [ ${chksums[0]}=$SHORT_V1_FIRST -a ${chksums[1]}=$SHORT_V1_FIRST ]
-then
-	echo "FAILED"
-	echo " - v1: expected $SHORT_V1_FIRST, got ${chksums[0]}"
-	echo " - v2: expected $SHORT_V2_FIRST, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "short.wav (first track)... " $SHORT_V1_FIRST $SHORT_V2_FIRST ${chksums[@]}
 
 chksums=($(../accuraterip -l short.wav))
-echo -n "Test for short.wav (last track)... "
-if ! [ ${chksums[0]}=$SHORT_V1_LAST -a ${chksums[1]}=$SHORT_V1_LAST ]
-then
-	echo "FAILED"
-	echo " - v1: expected $SHORT_V1_LAST, got ${chksums[0]}"
-	echo " - v2: expected $SHORT_V2_LAST, got ${chksums[1]}"
-else
-	echo "OK"
-fi
+comp "short.wav (last track)... " $SHORT_V1_LAST $SHORT_V2_LAST ${chksums[@]}
+
+chksums=($(../accuraterip -f -l short.wav))
+comp "short.wav (single track)... " $SHORT_V1_SINGLE $SHORT_V2_SINGLE ${chksums[@]}
+
+
+# check long file subset
+chksums=($(../accuraterip full.wav 30,20 ))
+comp "subset of full.wav (middle track)... " $SHORT_V1_NORMAL $SHORT_V2_NORMAL ${chksums[@]}
+
+chksums=($(../accuraterip -f full.wav 30,20))
+comp "subset of full.wav (first track)... " $SHORT_V1_FIRST $SHORT_V2_FIRST ${chksums[@]}
+
+chksums=($(../accuraterip -l full.wav 30,20))
+comp "subset of full.wav (last track)... " $SHORT_V1_LAST $SHORT_V2_LAST ${chksums[@]}
+
+chksums=($(../accuraterip -f -l full.wav 30,20))
+comp "subset of full.wav (single track)... " $SHORT_V1_SINGLE $SHORT_V2_SINGLE ${chksums[@]}
 
 echo "Finished"
