86f79c085efab9ff5f3bc9d4d1a905d197482f32
Bas Zoetekouw <bas.zoetekouw@surf.nl>
2022-02-20 15:34:33 +0100

    cleanup

--- accuraterip/cmdline.c
+++ accuraterip/cmdline.c
@@ -10,38 +10,38 @@ bool VERBOSE = false;
 
 int help(const char * const error, ...)
 {
-	if (error!=NULL)
-	{
-	    printf("ERROR: ");
+    if (error!=NULL)
+    {
+        printf("ERROR: ");
 
         va_list args;
         va_start(args, error);
         vprintf(error, args);
         va_end(args);
 
-		printf("\n\n");
-	}
-	printf("Syntax: accuraterip [-v] [-f] [-l] [-o <samples>] <filename.wav> [<start1>[,<length1>] [<start2>[,<length2>] ...]] \n");
-	printf("\n");
-	printf("Available options:\n");
-	printf("  --first (-f)   : track is first on disc (first 5 frames are ignored)\n");
-	printf("  --last (-l)    : track is last on disc (final 5 frames are ignored)\n");
-	printf("  --offset (-o)  : adjust offset of samples in audiofile (to correct for cd player offsets)\n");
-	printf("  --verbose (-v) : show verbose output\n");
-	printf("\n");
-	printf("<startN>  :  start time of Nth track in file (default: start of file)\n");
-	printf("<lengthN> :  length of Nth track in file (default: from <start> until the end of the file\n");
-	printf("\n");
-	printf("Start and length times can be specififed as:\n");
-	printf("  Ns for N samples, or\n");
+        printf("\n\n");
+    }
+    printf("Syntax: accuraterip [-v] [-f] [-l] [-o <samples>] <filename.wav> [<start1>[,<length1>] [<start2>[,<length2>] ...]] \n");
+    printf("\n");
+    printf("Available options:\n");
+    printf("  --first (-f)   : track is first on disc (first 5 frames are ignored)\n");
+    printf("  --last (-l)    : track is last on disc (final 5 frames are ignored)\n");
+    printf("  --offset (-o)  : adjust offset of samples in audiofile (to correct for cd player offsets)\n");
+    printf("  --verbose (-v) : show verbose output\n");
+    printf("\n");
+    printf("<startN>  :  start time of Nth track in file (default: start of file)\n");
+    printf("<lengthN> :  length of Nth track in file (default: from <start> until the end of the file\n");
+    printf("\n");
+    printf("Start and length times can be specififed as:\n");
+    printf("  Ns for N samples, or\n");
     printf("  N  for N seconds, or\n");
     printf("  [m:]s:f for minutes, seconds and frames\n");
-	printf("  (1 frame  is 1/75 seconds, or 588 samples)\n");
+    printf("  (1 frame  is 1/75 seconds, or 588 samples)\n");
 
-	if (error==NULL)
-		exit(0);
-	else
-		exit(1);
+    if (error==NULL)
+        exit(0);
+    else
+        exit(1);
 }
 
 opts_t parse_args(const int argc, char ** argv) {
@@ -142,7 +142,7 @@ opts_t parse_args(const int argc, char ** argv) {
     }
 
 
-	debug("Input filename: '%s'\n", opts.filename);
+    debug("Input filename: '%s'\n", opts.filename);
     debug("Number of tracks: %d\n", opts.num_tracks);
     for (unsigned int i=0; i<opts.num_tracks; i++)
     {
@@ -163,5 +163,5 @@ opts_t parse_args(const int argc, char ** argv) {
                 is_last_track(track->track_type)  ? 'l' : '-' );
     }
 
-	return opts;
+    return opts;
 }
